3f78d6e3b00375ae571c0dc808873444a49a3292
Aleartulon <alelon25@gmail.com>
2025-02-07 11:52:04 +0100

    added path to checkpoint when is_coupled : [false, 'NODE']

--- bin/main.py
+++ bin/main.py
@@ -57,7 +57,7 @@ def main():
         conv_encoder = Convolutional_Encoder(initial_information['dim_input'], model_information['kernel_enc'], model_information['filters_enc'], model_information['stride_enc'], model_information['input_output_dfnn'], model_information['latent_dim'], model_information['final_and_initial_activation'])
         f = F_Latent(model_information['parameter_information'], initial_information['dim_parameter'], model_information['latent_dim'], model_information['n_neurons_f'], model_information['n_layers_f'], model_information['n_FiLM_conditioning'])
         conv_decoder = Convolutional_Decoder(initial_information['dim_input'], model_information['kernel_deco'], model_information['filters_deco'], model_information['stride_dec'], model_information['latent_dim'], model_information['input_output_dfnn'], final_reduction, model_information['final_and_initial_activation'])
-        checkpoint = tc.load(initial_information['PATH']+'/checkpoint/check.pt', map_location=device, weights_only=True)
+        checkpoint = tc.load(initial_information['path_trained_AE']+'/checkpoint/check.pt', map_location=device, weights_only=True)
 
         conv_encoder.load_state_dict(checkpoint['enco'])
         conv_decoder.load_state_dict(checkpoint['dec'])
